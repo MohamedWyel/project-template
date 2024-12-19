8d63471eeda58f55bc2135bca0fb0048232a7963
Ahmed-Fahmy21 <ahmedmfahmy.21@gmail.com>
2024-12-19 19:08:29 +0200

    Initiate Project

---
+++ project-template/frontend/grand-continental-hotel/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Register from './components/Register.js';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the Hotel Website</h1>
+      <Register />
+    </div>
+  );
+}
+
+export default App;
